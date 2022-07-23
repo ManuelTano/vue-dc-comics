fd558f443b296566ec0e15120da5729483e541f3
ManuelTano <manueltano98@gmail.com>
2022-07-23 12:35:35 +0200

    add bonus static

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,10 +1,46 @@
 <template>
-<main>
-  <div class="container">
-    <h2>--> Content goes here </h2>
-  </div>
-</main>
+  <main>
+    <div class="container">
+      <h2>--> Content goes here</h2>
+    </div>
 
+    <div class="shop">
+      <div class="container">
+        <ul>
+          <li>
+            <img
+              src="../assets/img/buy-comics-digital-comics.png"
+              alt="buy-comics-digital-comics"
+            /><span>DIGITAL COMICS</span>
+          </li>
+          <li>
+            <img
+              src="../assets/img/buy-comics-merchandise.png"
+              alt="buy-comics-merchandise"
+            /><span>DC MERCHANDISE</span>
+          </li>
+          <li>
+            <img
+              src="../assets/img/buy-comics-shop-locator.png"
+              alt="buy-comics-shop-locator"
+            /><span>SUBSCRIPTION</span>
+          </li>
+          <li>
+            <img
+              src="../assets/img/buy-comics-subscriptions.png"
+              alt="buy-comics-subscriptions"
+            /><span>COMIC SHOP LOCATOR</span>
+          </li>
+          <li>
+            <img
+              src="../assets/img/buy-dc-power-visa.svg"
+              alt="buy-dc-power-visa" class="visa"
+            /><span>DC POWER VISA</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script >
@@ -13,14 +49,43 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 @import "../assets/sass/vars";
 
-  h2 {
-    color: $white;
-  }
+h2 {
+  color: $white;
+}
 
-  main {
-    padding: 25px;
-  }
+.shop {
+  background-color: $blue;
+  padding: 25px;
+}
+
+ul {
+  display: flex;
+  justify-content: center;
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  cursor: pointer;
+}
+
+ul li {
+  align-items: center;
+  display: flex;
+}
+
+img {
+  width: 15%;
+}
+
+span {
+  color: $white;
+  margin-left: 10px;
+}
+
+.visa {
+  width: 50px;
+}
 </style>
+ 
