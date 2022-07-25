ed84b2320d3244f41ebdce0de486c063bcc11642
ManuelTano <manueltano98@gmail.com>
2022-07-25 16:58:43 +0200

    added props

--- src/components/MyCards.vue
+++ src/components/MyCards.vue
@@ -1,17 +1,38 @@
 <template>
- 
+  <ul class="card">
+    <li v-for="(card, i) in cards" :key="i">
+      <a href="#">
+        <img :src="card.thumb" :alt="card.series" />
+        <h5>{{ card.series }}</h5>
+      </a>
+    </li>
+  </ul>
 </template>
 
 <script>
-
 export default {
   name: "MyCards",
-  components: {
-  
+  components: {},
+  props: {
+    cards: Array,
   },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../assets/sass/vars";
+
+.card {
+  display: flex;
+  flex-wrap: wrap;
+  li {
+    width: calc(100% / 6);
+  }
+}
+
+a {
+  text-decoration: none;
+  color: white;
+}
 
 </style>
